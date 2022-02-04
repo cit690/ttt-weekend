173adc7d48bace7c95c3f6ef21f6c8522fce4997
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-02-04 15:54:08 -0500

    lab edits

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
+const winningCombos = [
+  {combo1: [sq0, sq1, sq2]},
+  {combo2: [sq0, sq4, sq8]},
+  {combo3: [sq0, sq3, sq6]},
+  {combo4: [sq1, sq4, sq7]},
+  {combo5: [sq2, sq4, sq6]},
+  {combo6: [sq2, sq5, sq8]},
+  {combo7: [sq3, sq4, sq5]},
+  {combo8: [sq6, sq7, sq8]}
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
-// let squares = []
+
 
 let turn
 let winner 
 
 
 /*------------------------ Cached Element References ------------------------*/
-const allSquare = document.querySelectorAll('.square')
-
+const allSquare = Array.from(document.querySelectorAll('.square'))
+console.log(allSquare)
 
 const h2Message = document.getElementById('message')
 
@@ -24,15 +33,16 @@ const resetBtn = document.getElementById('reset')
 
 /*-------------------------------- Functions --------------------------------*/
 
+
 function init(){
   
-  let squares = [sq0 = null, sq1 = null, sq2 = null, sq3 = null, sq4 = null, sq5 = null, sq6 = null, sq7 = null, sq8 = null]
-  
-  
-  
+  boardArray = [null, null, null, null, null, null, null, null, null,]
+  resetBtn.setAttribute("hidden", true)
   playerX = 1
   playerO = -1
-  winner = null
+  square = []
+  isWinner = null
+  h2Message = 
 
 	    // This represents that there is no winner or tie yet. 
 	    // The winner variable will hold the player value (1 or -1) if there's a winner. 
@@ -46,30 +56,18 @@ function handleClick(evt){
 }
 
 
-
- const winningCombos = {
-  combo1: [sq0, sq1, sq2],
-  combo2: [sq0, sq4, sq8],
-  combo3: [sq0, sq3, sq6],
-  combo4: [sq1, sq4, sq7],
-  combo5: [sq2, sq4, sq6],
-  combo6: [sq2, sq5, sq8],
-  combo7: [sq3, sq4, sq5],
-  combo8: [sq6, sq7, sq8]
- }
-
-
 function render(){
 // 3.3.1) Loop over the board array (which represents the squares on the page), and for each iteration:
-allSquare.forEach(function (square){
-  return square[i]
-})
+for(let i = 0; i < boardArray.length; i++){
+  if (boardArray[i] === 1){
+    square[i].textComment = "X"
+  } else if (boardArray[i] === -1){
+    square[i].textComment = "O"
+  } else {boardArray[i] === null}
+  square[i] = null
+}
+console.log(boardArray)
 
-if (square[i] === 1){
-  return "X"
-} else if (square[i] === -1){
-  return "O"
-} else { winner === null}
 
 
 // const squareIndex = squares.findIndex()
