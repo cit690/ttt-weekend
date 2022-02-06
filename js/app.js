b33e69207675b19fc0b6a85c0d26576138efc963
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-02-06 18:46:28 -0500

    trying

--- js/app.js
+++ js/app.js
@@ -1,48 +1,61 @@
 /*-------------------------------- Constants --------------------------------*/
 const players = {
-  '1': {
-    name: '',
-    score: 0
-  },
-  '-1': {
-    name: '',
-    score: 0
-  }
+  playerX: 1,
+  playerO: -1
 };
 
-const winningCombos = [
-  {combo1: [sq0, sq1, sq2]},
-  {combo2: [sq0, sq4, sq8]},
-  {combo3: [sq0, sq3, sq6]},
-  {combo4: [sq1, sq4, sq7]},
-  {combo5: [sq2, sq4, sq6]},
-  {combo6: [sq2, sq5, sq8]},
-  {combo7: [sq3, sq4, sq5]},
-  {combo8: [sq6, sq7, sq8]}
-]
 
+const winningCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
+// const winningCombos = [
+//   {combo1: [sq0, sq1, sq2]},
+//   {combo2: [sq0, sq4, sq8]},
+//   {combo3: [sq0, sq3, sq6]},
+//   {combo4: [sq1, sq4, sq7]},
+//   {combo5: [sq2, sq4, sq6]},
+//   {combo6: [sq2, sq5, sq8]},
+//   {combo7: [sq3, sq4, sq5]},
+//   {combo8: [sq6, sq7, sq8]}
+// ]
+// console.log(winningCombos)
 /*---------------------------- Variables (state) ----------------------------*/
 
 
 let turn
-let winner 
+let isWinner 
 
 
 /*------------------------ Cached Element References ------------------------*/
 const allSquare = Array.from(document.querySelectorAll('.square'))
-
+console.log(allSquare)
 const h2Message = document.getElementById('message')
+
 
 const resetBtn = document.getElementById('reset')
 
 
+
 /*----------------------------- Event Listeners -----------------------------*/
-document.querySelector('div').addEventListener('click', handleClick)
+document.querySelector('.square').addEventListener('click', handleClick)
 
 
 function handleClick(evt){
-  evt.target.id = ""
-  console.log(evt)
+  let sqIdx = parseInt(evt.target.id.replace('sq', ''))
+  if((boardArray[sqIdx]) === 1 || -1){
+    return
+  }
+  if (isWinner !== null) {
+    return
+  }
+  turn = turn * -1
 }
 
  
@@ -53,13 +66,13 @@ function handleClick(evt){
 function init(){
   
   boardArray = [null, null, null, null, null, null, null, null, null]
+  console.log(boardArray)
   resetBtn.setAttribute("hidden", true)
-  playerX = 1
-  playerO = -1
   turn = 1
-  square = []
   isWinner = null
+  console.log(isWinner)
   isTie = 'T'
+  console.log(isTie)
   h2Message.textContent = ""
 
 	    // This represents that there is no winner or tie yet. 
@@ -70,8 +83,6 @@ function init(){
 }
 init()
 
-
-
 function render(){
 // 3.3.1) Loop over the board array (which represents the squares on the page), and for each iteration:
 boardArray.forEach((square, i)=>{
@@ -81,14 +92,12 @@ boardArray.forEach((square, i)=>{
   } else if (square === -1){
     allSquare[i].textContent = "O"
     allSquare[i].style.color = 'purple'
-  } else {square === null
+  } else { 
     allSquare[i].textContent = ""}
 })
 
-
-
 if(isWinner !== null){
-  h2Message.textContent = `Game still in process, ${1 || -1} goes next`
+  h2Message.textContent = `Game still in process, ${turn === 1 ? 'X' : 'O'} goes next`
 } else if (isWinner = 'T'){
   h2Message.textContent = `It's a tie!`
   h2Message.style.color = '#2ec4b6'
@@ -96,11 +105,15 @@ if(isWinner !== null){
   h2Message.textContent = `Congrats ${1 || -1}! You win!`
 }
 
+// getWinner()
 }
 
 
-function getWinner(winner) {
-const winner = winningCombos.reduce(function(){
+// function getWinner() {
+// // const winner = winningCombos.reduce(function(){
+// // })
 
-})
-}
+//   if (boardArray[winningCombos[0]]+boardArray[winningCombos[1]]+boardArray[winningCombos[2]] === 3){
+//     h2Message.textContent === `X wins!`
+//   }
+// }
